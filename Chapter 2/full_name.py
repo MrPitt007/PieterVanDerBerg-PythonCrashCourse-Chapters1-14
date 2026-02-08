893bf0e6bf5ea92692046134dddf033a0a245adc
MrPitt007 <mvanderberg190@gmail.com>
2026-02-08 18:43:37 +0100

    Update full_name.py

--- Chapter 2/full_name.py
+++ Chapter 2/full_name.py
@@ -1,1 +1,11 @@
+# full_name.py
 
+# 1) Store first and last name in variables
+first_name = "ada"
+last_name = "lovelace"
+
+# 2) Build a full name using an f-string
+full_name = f"{first_name} {last_name}"
+
+# 3) Print the full name
+print(full_name)
