fbf6b04c81465946b29d25dcb83669a866079510
MrPitt007 <mvanderberg190@gmail.com>
2026-02-09 14:03:06 +0100

    Delete Chapter 2 /exercise_2_6_famous_quote_2.py

--- Chapter 2 /exercise_2_6_famous_quote_2.py
+++
@@ -1,7 +0,0 @@
-# Exercise 2-6: Famous Quote 2
-
-famous_person = "Albert Einstein"
-quote = "A person who never made a mistake never tried anything new."
-
-message = f'{famous_person} once said, "{quote}"'
-print(message)
