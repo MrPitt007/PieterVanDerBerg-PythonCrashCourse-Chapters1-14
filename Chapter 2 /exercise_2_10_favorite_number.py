10c052d1a220da36727561b2d15b088dba936fa5
MrPitt007 <mvanderberg190@gmail.com>
2026-02-09 14:44:56 +0100

    Update exercise_2_10_favorite_number.py

--- Chapter 2 /exercise_2_10_favorite_number.py
+++ Chapter 2 /exercise_2_10_favorite_number.py
@@ -1,1 +1,7 @@
+# Exercise 2-10: Favorite Number
+# Store your favorite number in a variable, then create a message
+# revealing your favorite number.
 
+favorite_number = 7
+message = f"My favorite number is {favorite_number}!"
+print(message)
