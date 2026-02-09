e6983f592224417bcc9b8ce31b6f6a89834a26d8
MrPitt007 <mvanderberg190@gmail.com>
2026-02-09 14:51:40 +0100

    Create exercise_2_11_adding_comments.py

---
+++ Chapter 2/exercise_2_11_adding_comments.py
@@ -0,0 +1,16 @@
+# Exercise 2-11: Adding Comments
+# Author: Pieter Van Der Berg
+# Date: 2026-02-09
+# This program shows two commented examples from previous exercises.
+
+# --- Example 1: Favorite Number (from exercise 2-10) ---
+# This prints a message revealing my favorite number.
+favorite_number = 7
+print(f"My favorite number is {favorite_number}!")
+
+# --- Example 2: Number Eight (from exercise 2-9) ---
+# Each of these operations results in the number 8.
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2)
