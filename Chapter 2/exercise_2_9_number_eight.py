0c8d63d015993624c4fb50f9aef693d54d6890c0
MrPitt007 <mvanderberg190@gmail.com>
2026-02-09 14:43:54 +0100

    Create exercise_2_9_number_eight.py

---
+++ Chapter 2/exercise_2_9_number_eight.py
@@ -0,0 +1,8 @@
+# Exercise 2-9: Number Eight
+# Write addition, subtraction, multiplication, and division operations
+# that each result in the number 8.
+
+print(5 + 3)     # addition
+print(10 - 2)    # subtraction
+print(4 * 2)     # multiplication
+print(16 / 2)    # division
