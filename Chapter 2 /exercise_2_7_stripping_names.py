2a467c4c27a4001763b1cef5e68a8413d842f43a
MrPitt007 <mvanderberg190@gmail.com>
2026-02-09 14:04:56 +0100

    Create exercise_2_7_stripping_names.py

---
+++ Chapter 2 /exercise_2_7_stripping_names.py
@@ -0,0 +1,8 @@
+# Exercise 2-7: Stripping Names
+
+name = "\t Pieter Van Der Berg \n"
+
+print("Original:", repr(name))
+print("lstrip():", repr(name.lstrip()))
+print("rstrip():", repr(name.rstrip()))
+print("strip():", repr(name.strip()))
