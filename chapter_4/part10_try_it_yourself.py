9bb986c8f08e91fb28f74cfefe1c6404508c03b1
MrPitt007 <mvanderberg190@gmail.com>
2026-02-12 14:39:31 +0100

    Add exercises for pizzas and animals

---
+++ chapter_4/part10_try_it_yourself.py
@@ -0,0 +1,21 @@
+# Chapter 4 – Part 10: Try It Yourself (Exercises 4-1 and 4-2)
+# ------------------------------------------------------------
+
+# 4-1. Pizzas
+pizzas = ['pepperoni', 'margherita', 'bbq chicken']
+
+print("Exercise 4-1: Pizzas\n")
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("\nI really love pizza!\n")
+
+
+# 4-2. Animals
+animals = ['dog', 'cat', 'hamster']
+
+print("Exercise 4-2: Animals\n")
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print("\nAny of these animals would make a great pet!")
