409852b14c8179601930edd0e0de68f56274e581
MrPitt007 <mvanderberg190@gmail.com>
2026-02-16 10:57:13 +0100

    Create part17_try_it_yourself_4_10_to_4_12.py

---
+++ Chapter_4/part17_try_it_yourself_4_10_to_4_12.py
@@ -0,0 +1,63 @@
+# part17_try_it_yourself_4_10_to_4_12.py
+# Python Crash Course – Chapter 4
+# Exercises 4-10, 4-11, 4-12
+
+# ------------------------------------------
+# 4-10: Slices
+# ------------------------------------------
+
+print("\n=== Exercise 4-10: Slices ===")
+
+items = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+print("The first three items in the list are:")
+print(items[:3])
+
+print("\nThree items from the middle of the list are:")
+mid_start = len(items) // 2 - 1
+print(items[mid_start:mid_start+3])
+
+print("\nThe last three items in the list are:")
+print(items[-3:])
+
+# ------------------------------------------
+# 4-11: My Pizzas, Your Pizzas
+# ------------------------------------------
+
+print("\n=== Exercise 4-11: My Pizzas, Your Pizzas ===")
+
+my_pizzas = ['margherita', 'pepperoni', 'hawaiian']
+friend_pizzas = my_pizzas[:]   # slice copy
+
+# Add different pizzas
+my_pizzas.append('diavola')
+friend_pizzas.append('bbq chicken')
+
+print("My pizzas are:")
+for pizza in my_pizzas:
+    print(" •", pizza.title())
+
+print("\nMy friend's pizzas are:")
+for pizza in friend_pizzas:
+    print(" •", pizza.title())
+
+# ------------------------------------------
+# 4-12: More Loops
+# ------------------------------------------
+
+print("\n=== Exercise 4-12: More Loops ===")
+
+foods = ['sushi', 'taco', 'pasta']
+friend_foods = foods[:]  # slice copy
+
+foods.append('ramen')
+friend_foods.append('burrito')
+
+print("My favorite foods are:")
+for food in foods:
+    print(" •", food.title())
+
+print("\nMy friend's favorite foods are:")
+for food in friend_foods:
+    print(" •", food.title())
+
