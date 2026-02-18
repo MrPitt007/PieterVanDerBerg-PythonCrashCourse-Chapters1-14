2933cf3b8f73e9d3cc5c0ca5d487b3a38ba5b1d4
MrPitt007 <mvanderberg190@gmail.com>
2026-02-18 11:41:12 +0100

    Chapter 6/page97_lookup_values_and_formatting.py

---
+++ Chapter 6/page97_lookup_values_and_formatting.py
@@ -0,0 +1,13 @@
+# favorite_languages.py
+
+# A dictionary mapping people to their favorite programming languages.
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+# Accessing a value from the dictionary.
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
