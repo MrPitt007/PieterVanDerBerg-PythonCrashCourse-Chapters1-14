6279a3ec8d929501850a436d9ea4f88ee90c84d5
MrPitt007 <mvanderberg190@gmail.com>
2026-02-18 19:20:53 +0100

    Chapter 6/ page110_dictionary_in_a_dictionary.py

---
+++ Chapter 6/page110_dictionary_in_a_dictionary.py
@@ -0,0 +1,28 @@
+ Chapter 6/page110_dictionary_in_a_dictionary.py
+
+
+ A dictionary containing multiple user dictionaries
+
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    },
+}
+
+ Loop through each user account
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+
+     Extract the inner dictionary’s values
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
