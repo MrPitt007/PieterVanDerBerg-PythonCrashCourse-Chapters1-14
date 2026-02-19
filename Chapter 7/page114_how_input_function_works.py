a5ec42c25fc1988010a6887d6ab9b4192c59861f
MrPitt007 <mvanderberg190@gmail.com>
2026-02-19 08:13:15 +0100

    Chapter 7/page114_how_input_function_works.py

---
+++ Chapter 7/page114_how_input_function_works.py
@@ -0,0 +1,15 @@
+# Chapter 7/page114_how_input_function_works.py
+
+# -------------------------------------------------------------
+# Basic use of input(): repeat whatever the user types
+# -------------------------------------------------------------
+
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+# -------------------------------------------------------------
+# Writing clear prompts
+# -------------------------------------------------------------
+
+name = input("Please enter your name: ")
+print(f"Hello, {name}!")
