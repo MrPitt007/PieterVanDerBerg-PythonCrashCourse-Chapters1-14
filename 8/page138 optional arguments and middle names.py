162db6158fc60446211c574764559543781893a0
MrPitt007 <mvanderberg190@gmail.com>
2026-02-20 09:47:14 +0100

    Chapter 8/page139 optional arguments and returning a dictionary.py

--- 8/page138 optional arguments and middle names.py
+++ 8/page138 optional arguments and middle names.py
@@ -1,5 +1,4 @@
 
-
 def get_formatted_name_full(first_name, middle_name, last_name):
     """Return a full name including first, middle, and last."""
     full_name = f"{first_name} {middle_name} {last_name}"
