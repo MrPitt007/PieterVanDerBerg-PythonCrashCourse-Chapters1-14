5e81d382096ee4a099799d6a01e87ce8e107f46b
MrPitt007 <mvanderberg190@gmail.com>
2026-02-20 08:23:58 +0100

    Chapter 8/page131 exercises 8-1 to 8-2 and_summary.py

---
+++ Chapter 8/page131 exercises 8-1 to 8-2 and_summary.py
@@ -0,0 +1,8 @@
+
+def greet_user(username):
+    """Display a personalized greeting."""
+    print(f"Hello, {username.title()}!")
+
+# Example calls (from book text)
+greet_user('jesse')
+greet_user('sarah')
