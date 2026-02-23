c2201f3ac5dd0b7e27e0e927b228023123772d00
MrPitt007 <mvanderberg190@gmail.com>
2026-02-23 12:21:45 +0100

    Chapter 12/235 alien invasion.py

---
+++ Chapter 12/235 alien invasion.py
@@ -0,0 +1,52 @@
+ Chapter 12/235 alien_invasion.py
+
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+
+         Load settings
+        self.settings = Settings()
+
+        # Screen settings
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+         Create a ship instance (from page 235)
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+             Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+             Redraw the screen with background color
+            self.screen.fill(self.settings.bg_color)
+
+             Draw the ship (from page 235)
+            self.ship.blitme()
+
+             Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+             Control frame rate
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
