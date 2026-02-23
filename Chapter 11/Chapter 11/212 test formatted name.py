ba1dad279cd42fc1bc6f9e9520741a634a30b416
MrPitt007 <mvanderberg190@gmail.com>
2026-02-23 10:19:17 +0100

    Delete Chapter 11/Chapter 11 directory

--- Chapter 11/Chapter 11/212 test formatted name.py
+++
@@ -1,8 +0,0 @@
-
-from chapter 11 211 testing a function import get ormatted name
-
-def test_first_last_name():
-    """Test that a first and last name are formatted correctly."""
-    formatted_name = get_formatted_name('janis', 'joplin')
-    assert formatted_name == 'Janis Joplin'
-``
