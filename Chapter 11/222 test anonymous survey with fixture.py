e071b54b19848f84babbec5e39a01995592c272b
MrPitt007 <mvanderberg190@gmail.com>
2026-02-23 11:40:38 +0100

    Chapter 11/222 test anonymous survey with fixture.py

---
+++ Chapter 11/222 test anonymous survey with fixture.py
@@ -0,0 +1,23 @@
+ Chapter 11/222 test anonymous survey with fixture.py
+
+import pytest
+from chapter_11_218_anonymous_survey_class import AnonymousSurvey
+
+@pytest.fixture
+def language_survey():
+    """A survey that will be available to all test functions."""
+    question = "What language did you first learn to speak?"
+    return AnonymousSurvey(question)
+
+def test_store_single_response(language_survey):
+    """Test that a single response is stored properly."""
+    language_survey.store_response('English')
+    assert 'English' in language_survey.responses
+
+def test_store_three_responses(language_survey):
+    """Test that three individual responses are stored properly."""
+    responses = ['English', 'Spanish', 'Mandarin']
+    for response in responses:
+        language_survey.store_response(response)
+    for response in responses:
+        assert response in language_survey.responses
