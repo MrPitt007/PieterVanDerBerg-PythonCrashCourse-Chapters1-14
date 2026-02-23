2d0ea6f4d8c72299f5529b464db033e74a658f5e
MrPitt007 <mvanderberg190@gmail.com>
2026-02-23 12:41:05 +0100

    Chapter 11/242 settings.py

---
+++ Chapter 12/242 settings.py
@@ -0,0 +1,14 @@
+# Chapter 12/242 settings.py
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings (page 242)
+        self.ship_speed = 1.5
