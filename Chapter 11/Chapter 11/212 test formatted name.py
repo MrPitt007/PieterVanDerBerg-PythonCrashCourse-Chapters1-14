c3abde938cf79cd8aac3e6fa6faf9eed3391ff0e
MrPitt007 <mvanderberg190@gmail.com>
2026-02-23 10:17:14 +0100

    Chapter 11/212 test formatted name.py

---
+++ Chapter 11/Chapter 11/212 test formatted name.py
@@ -0,0 +1,8 @@
+
+from chapter_11_211_testing_a_function import get_formatted_name
+
+def test_first_last_name():
+    """Test that a first and last name are formatted correctly."""
+    formatted_name = get_formatted_name('janis', 'joplin')
+    assert formatted_name == 'Janis Joplin'
+``
