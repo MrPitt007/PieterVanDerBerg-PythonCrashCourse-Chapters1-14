401c1f1f98089ecaf0cf8c3317cdfe9b84e7523e
MrPitt007 <mvanderberg190@gmail.com>
2026-02-23 10:24:22 +0100

    Chapter 11/ 214 failing test example.py

---
+++ Chapter 11/214 failing test example.py
@@ -0,0 +1,6 @@
+ Chapter 11/214 failing test example.py
+
+def get_formatted_name(first, middle, last):
+    """Generate a neatly formatted full name."""
+    full_name = f"{first} {middle} {last}"
+    return full_name.title()
