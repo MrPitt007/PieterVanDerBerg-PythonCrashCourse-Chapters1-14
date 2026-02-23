0d59de6baf9f76da0891951ca48064f502bd96eb
MrPitt007 <mvanderberg190@gmail.com>
2026-02-23 10:20:21 +0100

    Chapter 11/212 test formatted name.py

---
+++ Chapter 11/212 test formatted name.py
@@ -0,0 +1,7 @@
+ Chapter 11/212 test formatted name.py
+
+def test_first_last_name():
+    """Test that a first and last name are formatted correctly."""
+    formatted_name = get_formatted_name('janis', 'joplin')
+    assert formatted_name == 'Janis Joplin'
+``
