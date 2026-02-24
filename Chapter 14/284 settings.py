a4f1d76f09c833e087238001ca53f4a0b4f45687
MrPitt007 <mvanderberg190@gmail.com>
2026-02-24 12:46:27 +0100

    Chapter 14/284 settings.py

---
+++ Chapter 14/284 settings.py
@@ -0,0 +1,41 @@
+# Chapter 14/284 settings.py
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's static settings."""
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # Alien settings
+        self.fleet_drop_speed = 10
+
+        # PAGE 284: how quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        # Initialize settings that can change dynamically
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+
+        # Speeds
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+
+        # fleet_direction of 1 means right; -1 means left
+        self.fleet_direction = 1
